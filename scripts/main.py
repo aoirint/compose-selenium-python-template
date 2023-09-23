8f016a6b38395cca01215053e395e55f108fc61f
aoirint <aoirint@gmail.com>
2023-09-23 17:34:29 +0900

    poetry run pysen run format

--- scripts/main.py
+++ scripts/main.py
@@ -1,22 +1,23 @@
 import os
 import time
+
 from selenium import webdriver
 
-selenium_url = os.environ['SELENIUM_URL']
-website_url = 'https://example.com'
+selenium_url = os.environ["SELENIUM_URL"]
+website_url = "https://example.com"
 
 options = webdriver.ChromeOptions()
-options.add_argument('--kiosk')
+options.add_argument("--kiosk")
 
 driver = webdriver.Remote(
-  command_executor=selenium_url,
-  options=options,
+    command_executor=selenium_url,
+    options=options,
 )
 
 driver.get(website_url)
 
 time.sleep(3)
 
-driver.save_screenshot('screenshot.png')
+driver.save_screenshot("screenshot.png")
 
 driver.quit()
