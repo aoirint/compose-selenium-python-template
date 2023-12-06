3f60d45161f4f8ea8aaf1ebb3336d8f6123b223b
aoirint <aoirint@gmail.com>
2023-12-06 17:48:10 +0900

    arg parser

--- compose_selenium_python_template/cli.py
+++ compose_selenium_python_template/cli.py
@@ -1,12 +1,29 @@
 import os
 import time
+from argparse import ArgumentParser
 
 from selenium import webdriver
 
 
 def main() -> None:
-    selenium_url = os.environ["SELENIUM_URL"]
-    website_url = "https://example.com"
+    env_selenium_url = os.environ.get("SELENIUM_URL") or None
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--website_url",
+        type=str,
+        default="https://example.com",
+    )
+    parser.add_argument(
+        "--selenium_url",
+        type=str,
+        default=env_selenium_url,
+        required=env_selenium_url is None,
+    )
+    args = parser.parse_args()
+
+    selenium_url: str = args.selenium_url
+    website_url: str = args.website_url
 
     options = webdriver.ChromeOptions()
     options.add_argument("--kiosk")  # type: ignore[no-untyped-call]
